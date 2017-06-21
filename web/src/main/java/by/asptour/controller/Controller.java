190aae7f69e4c6e3f3840ae8d22452efae2faacf
Alexey Polegoshko <alexey.polegoshko@gmail.com>
2017-06-21 12:48:34 +0300

    Added method index()

--- web/src/main/java/by/asptour/controller/Controller.java
+++ web/src/main/java/by/asptour/controller/Controller.java
@@ -1,9 +1,18 @@
 package by.asptour.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by Алексей on 21.06.2017.
  */
 
 @org.springframework.stereotype.Controller
 public class Controller {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(){
+        System.out.println("Hello");
+        return "/success";
+    }
 }
