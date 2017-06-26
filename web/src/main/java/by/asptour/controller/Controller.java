c2ff3950514eeeea9516f037f9edaf649520df9f
Alexey Polegoshko <alexey.polegoshko@gmail.com>
2017-06-26 10:44:21 +0300

    Deleted default file template

--- web/src/main/java/by/asptour/controller/Controller.java
+++ web/src/main/java/by/asptour/controller/Controller.java
@@ -3,10 +3,6 @@ package by.asptour.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Created by Алексей on 21.06.2017.
- */
-
 @org.springframework.stereotype.Controller
 public class Controller {
 
