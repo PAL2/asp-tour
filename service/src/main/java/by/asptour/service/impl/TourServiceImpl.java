666c1cc79409aab4c2066518c2353d5dad2d540d
Alexey Polegoshko <alexey.polegoshko@gmail.com>
2017-06-27 21:57:03 +0300

    Added setter for tourRepository and method findByCountry

--- service/src/main/java/by/asptour/service/impl/TourServiceImpl.java
+++ service/src/main/java/by/asptour/service/impl/TourServiceImpl.java
@@ -1,14 +1,24 @@
 package by.asptour.service.impl;
 
 import by.asptour.entity.Tour;
+import by.asptour.repository.TourRepository;
 import by.asptour.service.TourService;
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class TourServiceImpl implements TourService {
 
+    private TourRepository tourRepository;
+
     @Override
     public List<Tour> findByCountry(String country) {
-        return null;
+        return Lists.newArrayList(tourRepository.findByCountry(country));
+    }
+
+    @Autowired
+    public void setTourRepository(TourRepository tourRepository) {
+        this.tourRepository = tourRepository;
     }
 }
