ca65bfcec1221e82cb5f51ead5e70ca24c5149ea
Alexey Polegoshko <alexey.polegoshko@gmail.com>
2017-06-27 21:40:20 +0300

    Deleted file template

--- service/src/main/java/by/asptour/service/impl/TourServiceImpl.java
+++ service/src/main/java/by/asptour/service/impl/TourServiceImpl.java
@@ -5,9 +5,6 @@ import by.asptour.service.TourService;
 
 import java.util.List;
 
-/**
- * Created by Алексей on 27.06.2017.
- */
 public class TourServiceImpl implements TourService {
 
     @Override
