f0c52edf5d151def59237a3d88fc70e873008265
Alexey Polegoshko <alexey.polegoshko@gmail.com>
2017-06-27 21:39:47 +0300

    Added TourServiceImpl

--- service/src/main/java/by/asptour/service/impl/TourServiceImpl.java
+++ service/src/main/java/by/asptour/service/impl/TourServiceImpl.java
@@ -1,7 +1,17 @@
-package by.asptour.impl;
+package by.asptour.service.impl;
+
+import by.asptour.entity.Tour;
+import by.asptour.service.TourService;
+
+import java.util.List;
 
 /**
  * Created by Алексей on 27.06.2017.
  */
 public class TourServiceImpl implements TourService {
+
+    @Override
+    public List<Tour> findByCountry(String country) {
+        return null;
+    }
 }
