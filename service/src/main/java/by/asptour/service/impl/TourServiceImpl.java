d46526f8af5e1a9eb9325eceaffdff102dafb469
Alexey Polegoshko <alexey.polegoshko@gmail.com>
2017-06-27 23:27:59 +0300

    Added annotations: @Repository, @Service, @Transactional

--- service/src/main/java/by/asptour/service/impl/TourServiceImpl.java
+++ service/src/main/java/by/asptour/service/impl/TourServiceImpl.java
@@ -5,10 +5,15 @@ import by.asptour.repository.TourRepository;
 import by.asptour.service.TourService;
 import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Repository
+@Service
+@Transactional
 public class TourServiceImpl implements TourService {
 
     private TourRepository tourRepository;
