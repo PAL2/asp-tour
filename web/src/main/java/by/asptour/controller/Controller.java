4f5ee965ca6004f60aa999c3ad6e4abbe5f318bf
Alexey Polegoshko <alexey.polegoshko@gmail.com>
2017-06-29 20:50:43 +0300

    Added methods: getLoginPage and getLogoutPage

--- web/src/main/java/by/asptour/controller/Controller.java
+++ web/src/main/java/by/asptour/controller/Controller.java
@@ -3,10 +3,15 @@ package by.asptour.controller;
 import by.asptour.entity.Tour;
 import by.asptour.service.TourService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @org.springframework.stereotype.Controller
@@ -116,6 +121,18 @@ public class Controller {
     @RequestMapping(value = "picking", method = RequestMethod.GET)
     public String picking() {
         return "picking";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String getLoginPage() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String getLogoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) new SecurityContextLogoutHandler().logout(request, response, authentication);
+        return "redirect:/";
     }
 
     @Autowired
