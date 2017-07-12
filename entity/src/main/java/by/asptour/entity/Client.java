f772099515601ad36ab07c80d5e851bd7e2baab5
Alexey Polegoshko <alexey.polegoshko@gmail.com>
2017-07-12 13:15:07 +0300

    Added class Client

---
+++ entity/src/main/java/by/asptour/entity/Client.java
@@ -0,0 +1,81 @@
+package by.asptour.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "client")
+public class Client implements Serializable {
+    private Integer id;
+    private String name, phone, definition, note;
+    private int version;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Column(name = "NAME")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "PHONE")
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Column(name ="DEFINITION")
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    @Column(name = "NOTE")
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    @Version
+    @Column(name = "VERSION")
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", definition='" + definition + '\'' +
+                ", note='" + note + '\'' +
+                ", version=" + version +
+                '}';
+    }
+}
