20fafc749970a7fab58c8d736bb7fa203cf1669c
Alexey Polegoshko <alexey.polegoshko@gmail.com>
2017-07-12 22:53:51 +0300

    Added the methods: save and findAll

--- service/src/main/java/by/asptour/service/BidService.java
+++ service/src/main/java/by/asptour/service/BidService.java
@@ -2,7 +2,13 @@ package by.asptour.service;
 
 import by.asptour.entity.Bid;
 
+import java.util.List;
+
 public interface BidService {
 
     Bid save(Bid bid);
+
+    void delete(Integer id);
+
+    List<Bid> findAll();
 }
--- service/src/main/java/by/asptour/service/impl/BidServiceImpl.java
+++ service/src/main/java/by/asptour/service/impl/BidServiceImpl.java
@@ -3,10 +3,13 @@ package by.asptour.service.impl;
 import by.asptour.entity.Bid;
 import by.asptour.repository.BidRepository;
 import by.asptour.service.BidService;
+import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 @Service
@@ -18,6 +21,16 @@ public class BidServiceImpl implements BidService {
     @Override
     public Bid save(Bid bid) {
         return bidRepository.save(bid);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        bidRepository.delete(id);
+    }
+
+    @Override
+    public List<Bid> findAll() {
+        return Lists.newArrayList(bidRepository.findAll());
     }
 
     @Autowired
